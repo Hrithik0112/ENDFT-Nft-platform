df24fd970cbae2edfa6f3c2e811b90d64d9a1514
Hrithik <slumdog4321@gmail.com>
2024-04-03 10:13:29 +0530

    Feat : Added Wallet Support Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Wallet from "@/components/Wallet";
 
 export default function Home() {
   return (
     <main className="px-20 bg-[#141944] pt-10">
       <Navbar />
       <Hero />
+      <Wallet />
     </main>
   );
 }
---
+++ components/Wallet.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Wallet = () => {
+  return (
+    <div className="flex flex-col justify-center max-w-[1100px] h-48 mt-14 mx-auto">
+      {/* upeer */}
+      <div className="h-20 flex justify-between items-center">
+        <img src="/wallets/MetaMask.png" alt="metamask" width={285} height={80} />
+        <img src="/wallets/TrustWallet.png" alt="TrustWallet" width={257} height={76} />
+        <img src="/wallets/WalletConnect.png" alt="WalletConnect" width={318} height={53} />
+      </div>
+      {/* lower */}
+      <div className="h-16 flex justify-around">
+        <img src="/wallets/Exodus.png" alt="Exodus" width={285} height={58} />
+        <img src="/wallets/Safepal.png" alt="Safepal" width={210} height={60} />
+      </div>
+    </div>
+  );
+};
+
+export default Wallet;
