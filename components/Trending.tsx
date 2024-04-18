e934953391ac777c2201db83f50061496ada5766
Hrithik <slumdog4321@gmail.com>
2024-04-18 23:30:10 +0530

    Trending Section completed

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -3,17 +3,23 @@ import TrendingNFTCard from "./TrendingNFTCard";
 
 const Trending = () => {
   return (
-    <div className=" flex flex-col">
-      <h2 className="font-semibold font-clash bg-gradient-to-r from-[#69EACB] via-[#EACCF8] to-[#6654F1] text-transparent bg-clip-text">
-        Trending NFTs
-      </h2>
-      <div className="flex overflow-x-hidden gap-5">
+    <div className=" flex flex-col mt-40">
+      <div className="flex justify-center items-center">
+
+      <span className="font-semibold font-clash bg-gradient-to-r text-[55px]  from-[#69EACB] via-[#EACCF8] to-[#6654F1] inline-block text-transparent bg-clip-text">
+              Trending NFTs
+            </span>
+      </div>
+      <div className="flex overflow-x-hidden gap-5 mt-10">
         <TrendingNFTCard />
         <TrendingNFTCard />
         <TrendingNFTCard />
         <TrendingNFTCard />
         <TrendingNFTCard />
       </div>
+      <div className="mt-14 self-center  bg-transparent text-white text-2xl font-semibold border rounded-[60px] flex justify-center items-center w-[197px] h-[62px]  ">
+            see more
+          </div>
     </div>
   );
 };
