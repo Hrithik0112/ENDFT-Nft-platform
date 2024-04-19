5e425364398d4a6a69cb74721b01a57494277a64
Hrithik <slumdog4321@gmail.com>
2024-04-19 23:34:13 +0530

    Fix: Wallet Support section

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Wallet = () => {
   return (
-    <div className="flex flex-col justify-center max-w-[1100px] h-48 mt-14 mx-auto">
+    <div className="flex flex-col justify-center max-w-[1100px] h-56 mt-14 mx-auto gap-8">
       {/* upeer */}
       <div className="h-20 flex justify-between items-center">
         <img src="/wallets/MetaMask.png" alt="metamask" width={285} height={80} />
@@ -10,7 +10,7 @@ const Wallet = () => {
         <img src="/wallets/WalletConnect.png" alt="WalletConnect" width={318} height={53} />
       </div>
       {/* lower */}
-      <div className="h-16 flex justify-around">
+      <div className="h-16 flex justify-around ">
         <img src="/wallets/Exodus.png" alt="Exodus" width={285} height={58} />
         <img src="/wallets/Safepal.png" alt="Safepal" width={210} height={60} />
       </div>
