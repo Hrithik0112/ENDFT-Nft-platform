99ecc09bff76d77c4b271276b92171cb0fc0ceb9
Hrithik <slumdog4321@gmail.com>
2024-04-23 00:05:22 +0530

    Fix: Fix some postional errors

--- components/CreateAndSell.tsx
+++ components/CreateAndSell.tsx
@@ -3,15 +3,15 @@ import CreateAndSellCard from './CreateAndSellCard'
 
 const CreateAndSell = () => {
   return (
-    <div className='flex flex-col items-center  border border-gray-200 rounded-xl'>
+    <div className='flex flex-col items-center  border border-gray-200 rounded-[40px] mt-20'>
         {/* heading */}
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mt-10">
             <span className="font-semibold font-clash bg-gradient-to-r text-[55px]  from-[#69EACB] via-[#EACCF8] to-[#6654F1] inline-block text-transparent bg-clip-text">
             Create and Sell Your NFTs
             </span>
         </div>
         {/* content */}
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-evenly items-center gap-10 mt-10'>
 
         <CreateAndSellCard/>
         <CreateAndSellCard/>
--- components/CreateAndSellCard.tsx
+++ components/CreateAndSellCard.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const CreateAndSellCard = () => {
   return (
-    <div className='h-[313px] w-[378px] flex flex-col gap-6 text-white'>
+    <div className='h-[313px] w-[378px] flex flex-col gap-6 text-white justify-center items-center'>
         <img src="/cardImage/Wallet.png" alt="image" width={64} height={60} />
         <h2 className='text-[28px] font-semibold '>
             Set up your Wallet
         </h2>
-        <p className='text-lg font-normal'>
+        <p className='text-lg font-normal text-center'>
         Once you’ve set up your wallet of choice, connect it to OpenSea by clicking the wallet icon in the top right corner. Learn about the wallets we support.
         </p>
     </div>
